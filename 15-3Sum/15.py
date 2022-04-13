d0a74df77e32e7769cb19dde15dd533f7cc89b7e
jackberrypassionfruit <pashayanjack@gmail.com>
2022-04-12 20:55:21 -0400

    idk

--- 15-3Sum/15.py
+++ 15-3Sum/15.py
@@ -3,26 +3,6 @@ Find all of the triplets between the 3 numbers that add up to 0
 """
 
 nums = [-1,0,1,2,-1,-4,-2,-3,3,0,4]
-trips = []
-length = range(len(nums))
-breakout = False
-
-# for i in length:
-#     for j in length:
-#         for k in length:
-#             a = nums[i]
-#             b = nums[j]
-#             c = nums[k]
-#             if a + b + c == 0 and i != j and j != k and i != k:
-#                 breakout = False
-#                 for trip in trips:
-#                     if a in trip and b in trip and c in trip:
-#                         breakout = True
-#                 if breakout:
-#                     continue
-#                 trips.append([a, b, c])
-
-
 
 nums.sort()
 print(nums)
